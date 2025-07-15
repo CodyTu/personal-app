0a9b15547b67aba33cfbdfd521515e1e91441e88
ctu4 <ctu4@uw.edu>
2025-07-14 17:29:50 -0700

    Started general structure of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard/> } />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Job Tracker Application</h1>
+
+        </div>
+    );
+}
